f0f67c41315ab98ecc99dc2bfd51856ad514b331
Felipe Barros <bcfelipe@icloud.com>
2022-05-19 23:33:23 -0300

    initial Project with basic navigation

---
+++ Marvel Comics/SecondScreenViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SecondScreenViewController.swift
+//  Marvel Comics
+//
+//  Created by Felipe Barros on 19/05/22.
+//
+
+import Foundation
---
+++ Marvel Comics/SecondViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.swift
+//  Marvel Comics
+//
+//  Created by Felipe Barros on 19/05/22.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Hero Detail"
+    }
+}
